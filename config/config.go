eb85caa5497d8d7c92d79160896451a51a326944
Fajar Rizky <56773371+fajar1411@users.noreply.github.com>
2023-04-06 03:32:43 +0700

    Update config.go

--- config/config.go
+++ config/config.go
@@ -40,13 +40,13 @@ func GetConfig() *AppConfig {
 func initConfig() *AppConfig {
 	var defaultConfig AppConfig
 
-// if _, exist := os.LookupEnv("SECRET"); !exist {
-	// 	if err := godotenv.Load(".env"); err != nil {
-	// 		log.Fatal(err)
+	// if _, exist := os.LookupEnv("SECRET"); !exist {
+	// 	if err := godotenv.Load("local.env"); err != nil {
+	// 		log.Println(err)
 	// 	}
 	// }
 
-	// // SECRET = os.Getenv("SECRET")
+	// SECRET = os.Getenv("SECRET")
 	cnvServerPort, err := strconv.Atoi(os.Getenv("SERVER_PORT"))
 	// fmt.Println("ini port", cnvServerPort)
 	if err != nil {
@@ -69,15 +69,6 @@ func initConfig() *AppConfig {
 	}
 	defaultConfig.DB_PORT = uint(cnvDBPort)
 	defaultConfig.JWT_SECRET = os.Getenv("JWT_SECRET")
-
-	// log.Println(` Env List :
-	//  - server port : `, defaultConfig.SERVER_PORT,
-	// 	` \n - db name : `, defaultConfig.DB_NAME,
-	// 	` \n - db username : `, defaultConfig.DB_USERNAME,
-	// 	` \n - db password : `, defaultConfig.DB_PASSWORD,
-	// 	` \n - db host : `, defaultConfig.DB_HOST,
-	// 	` \n - db port : `, defaultConfig.DB_PORT,
-	// 	` \n - jwt secret : `, defaultConfig.JWT_SECRET)
 
 	return &defaultConfig
 }
